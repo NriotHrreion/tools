fa1fd013bd124ef5f5c6a25b6d935ee4257d47a8
NriotHrreion <Nriot233_kuanggong@163.com>
2020-04-21 16:08:08 +0800

    Delete background.js

--- crx/Nocp Startpage/js/background.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.setBadgeText({text: "off"});
-chrome.browserAction.setBadgeBackgroundColor({color: [192, 0, 0, 111]});
-chrome.contextMenus.create({
-    title: "打开新的标签页",
-    onclick: function() {window.open("nstart.html");}
-});
